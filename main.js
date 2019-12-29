1f2daf5c62fc4f09766e0c0b42f8a7f71a0ab659
Andy Fischer <info@andyland.info>
2019-12-29 19:42:17 +0100

    First connection

--- main.js
+++ main.js
@@ -342,7 +342,7 @@ class AudiomatrixB2008 extends utils.Adapter {
 				if(in_msg.toLowerCase().substring(iStartPos+16,iStartPos+18)=='0a'){                                                                                              
 					var tmpMSG = in_msg.toLowerCase().substring(iStartPos,iStartPos+20);	//Checksum
 					in_msg = in_msg.slice(20);
-					parentThis.log.info('_processIncoming(); filtered:' + tmpMSG);
+					//parentThis.log.info('_processIncoming(); filtered:' + tmpMSG);
 //					parentThis.bWaitingForResponse = false;
 					parentThis._parseMSG(tmpMSG);
 					
