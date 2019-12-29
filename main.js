5dd08a01fcb1a43ae31a59cbe02ad3153dfc2ec7
Andy Fischer <info@andyland.info>
2019-12-29 19:37:46 +0100

    First connection

--- main.js
+++ main.js
@@ -367,9 +367,9 @@ class AudiomatrixB2008 extends utils.Adapter {
 	
 	_parseMSG(sMSG){
 		this.log.info("_parseMSG():" + sMSG);
-		if( sMSG.equals(this.toHexString(cmdBasicResponse)) ){
+		if( sMSG===this.toHexString(cmdBasicResponse) ){
 			this.log.info("_parseMSG(): Basic Response.");
-		}else if( sMSG.equals(this.toHexString(cmdTransmissionDone)) ){
+		}else if( sMSG===this.toHexString(cmdTransmissionDone) ){
 			this.log.info("_parseMSG(): TransmissionDone.");
 		}
 	}
