7c6b9c6fbad53055dd4eeaed1863857065289884
Andy Fischer <info@andyland.info>
2020-01-16 20:15:14 +0100

    process routing in parseMSG()

--- main.js
+++ main.js
@@ -365,8 +365,13 @@ class AudiomatrixB2008 extends utils.Adapter {
           var iValue = HexToFloat32(sValue, 16);
           //this.log.info("_parseMSG(): received inputGain from Matrix. Original Value:" + sValue.toString());
           iValue = map(iValue, -80, 0, 0, 100); //this.simpleMap(0, 100, iVal);
-          this.log.info('_parseMSG(): received inputGain for Channel ' + (iVal).toString()  + ' from Matrix. Processed Value:' + iValue.toString());
+          this.log.info('_parseMSG(): received gain for input ' + (iVal).toString()  + ' from Hardware. Processed Value:' + iValue.toString());
           this.setStateAsync("inputGain", { val: iValue, ack: true });
+        }else if((iCmd>=51)&&(iCmd<=58)){
+          this.log.info('_parseMSG(): received routing info. IN:' + (iVal).toString()  + ' OUT:' + (iCmd-50).toString());
+          //var sValue = sMSG.substring(8, 16);
+          //var iValue = HexToFloat32(sValue, 16);
+
         }
       } else if (iVal >= 7 && iVal <= 14) {
         //----Output....
@@ -380,7 +385,7 @@ class AudiomatrixB2008 extends utils.Adapter {
           var iValue = HexToFloat32(sValue, 16);
           //this.log.info("_parseMSG(): received outputGain from Matrix. Original Value:" + sValue.toString());
           iValue = map(iValue, -80, 0, 0, 100); //this.simpleMap(0, 100, iVal);
-          this.log.info('_parseMSG(): received outputGain for Channel ' + (iVal-7).toString()  + ' from Matrix. Processed Value:' + iValue.toString());
+          this.log.info('_parseMSG(): received gain for output ' + (iVal-7).toString()  + ' from Hardware. Processed Value:' + iValue.toString());
           this.setStateAsync("outputGain", { val: iValue, ack: true });
         }
       }
